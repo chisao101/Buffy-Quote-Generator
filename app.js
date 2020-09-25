37fee3130168a4e34138b5068c4971cfe0164abf
Mike Nealon <chisao101@gmail.com>
2020-09-25 06:02:58 -0400

    Finalized function
    Many changes:
    Get values from randomly picked object
    Place values in appropriate places
    Fixed bg image replacement

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ window.onload = generateQuote;
 let allQuotes = [
     {
         quotedText: "I might be love\'s bitch, but at least I\'m man enough to admit it.", 
-        quoteSpeaker: "~ Willam \"Spike\" Pratt"
-        
+        quoteSpeaker: "~ Willam \"Spike\" Pratt",
+        image: "url('./images/buffy-blue-bg.jpg')",        
     },
     {
         quotedText: "And If Giles Wants To Go After The, Uh, Fiend That Murdered His Girlfriend, I Say, \"Faster, Pussycat, Kill, Kill.\"", 
-        quoteSpeaker: "~ Xander Harris"
+        quoteSpeaker: "~ Xander Harris",
+        image: "url('./images/buffy-fan-art.jpg')",
+    },
+    {
+        quotedText: "That\'s me as a vampire? I\'m so skanky. And I think I\'m kind of gay.", 
+        quoteSpeaker: "~ Willow Rosenberg",
+        image: "url('./images/buffy-the-vampire-slayer-feature-1280x720.png')",
     },
 ]
     //"I might be love\'s bitch, but at least I\'m man enough to admit it.",
@@ -19,24 +25,41 @@ let allQuotes = [
  //   {speaker: '~ Daniel \"Oz\" Osbourne' quote: 'Well, a gathering is brie, mellow song stylings; shindig, dip, less mellow song stylings, perhaps a large amount of malt beverage; and hootenanny, well, it\'s chock full of hoot, just a little bit of nanny.'}
   //  {speaker: '~ Buffy Summers' quote: 'The hardest thing in this world is to live in it.'}
 ///    {speaker: 'William \"Spike\" Pratt,<br>AKA William The Bloody' quote: 'Out. For. A. Walk ...Bitch.'}
-//    {speaker: 'Willow Rosenberg' quote: 'That\'s me as a vampire? I\'m so skanky. And I think I\'m kind of gay.'}
+//    {speaker: '' quote: ''}
 
 
 
 
 //make variable from button
 const generateBtn = document.querySelector('.generator-btn');
+//Variable for .quote class
 let quote = document.querySelector('.quote');
-//let quotedText = allQuotes[0, 1];
+//Variable for .speaker class
+let speaker =document.querySelector('.speaker');
+//Variable for .image class
+let image = document.querySelector('.image');
 //Eventlistener for button
 generateBtn.addEventListener('click', generateQuote);
     //function to generate a quote
     function generateQuote(event){
         //generate random number and round to whole number based on total number of quotes in array
         let randomNumber = Math.floor(Math.random() * (allQuotes.length));
+        //Set random pick into quotedText variable 
         const quotedText = quote.innerHTML = allQuotes[randomNumber];
+        //Set object values into values variable
+        let values = Object.values(quotedText);
+        //Set quote value into quote div 
+        quote.innerHTML = values[0];
+        //Set speaker value into speaker div
+        speaker.innerHTML = values[1];
+        //Change backgorund image in image style
+        image.style.backgroundImage = values[2];
+        
 
-        console.log(allQuotes[randomNumber]);
+
+
+
+
     }
 
 
